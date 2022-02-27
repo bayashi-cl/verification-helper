83860ff7251ef2debe09fede525512a2110c7cea
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-02-28 06:20:59 +0900

    Optimize dependency resolution using `moduleinfo`.

--- onlinejudge_verify/languages/python.py
+++ onlinejudge_verify/languages/python.py
@@ -1,16 +1,11 @@
 # Python Version: 3.x
-import concurrent.futures
 import functools
 import pathlib
-import platform
+import modulefinder
 import sys
 import textwrap
 from logging import getLogger
-from typing import Any, Dict, List, Sequence, Tuple
-
-import importlab.environment
-import importlab.fs
-import importlab.graph
+from typing import Any, Dict, List, Sequence
 
 from onlinejudge_verify.languages.models import Language, LanguageEnvironment
 
@@ -36,7 +31,7 @@ class PythonLanguageEnvironment(LanguageEnvironment):
             # run {str(path)}
             env = dict(os.environ)
             if "PYTHONPATH" in env:
-                env["PYTHONPATH"] = basedir + os.pathsep + env["PYTHONPATH"] 
+                env["PYTHONPATH"] = basedir + os.pathsep + env["PYTHONPATH"]
             else:
                 env["PYTHONPATH"] = basedir  # set `PYTHONPATH` to import files relative to the root directory
             os.execve(sys.executable, [sys.executable, path], env=env)  # use `os.execve` to avoid making an unnecessary parent process
@@ -50,38 +45,20 @@ class PythonLanguageEnvironment(LanguageEnvironment):
 
 @functools.lru_cache(maxsize=None)
 def _python_list_depending_files(path: pathlib.Path, basedir: pathlib.Path) -> List[pathlib.Path]:
-    # compute the dependency graph of the `path`
-    env = importlab.environment.Environment(
-        importlab.fs.Path([importlab.fs.OSFileSystem(str(basedir.resolve()))]),
-        (sys.version_info.major, sys.version_info.minor),
-    )
-    try:
-        executor = concurrent.futures.ThreadPoolExecutor()
-        future = executor.submit(importlab.graph.ImportGraph.create, env, [str(path)])
-        if platform.uname().system == 'Windows':
-            timeout = 5.0  # 1.0 sec causes timeout on CI using Windows
-        else:
-            timeout = 1.0
-        res_graph = future.result(timeout=timeout)
-    except concurrent.futures.TimeoutError as e:
-        raise RuntimeError(f"Failed to analyze the dependency graph (timeout): {path}") from e
-    try:
-        node_deps_pairs = res_graph.deps_list()  # type: List[Tuple[str, List[str]]]
-    except Exception as e:
-        raise RuntimeError(f"Failed to analyze the dependency graph (circular imports?): {path}") from e
-    logger.debug('the dependency graph of %s: %s', str(path), node_deps_pairs)
-
     # collect Python files which are depended by the `path` and under `basedir`
     res_deps = []  # type: List[pathlib.Path]
-    res_deps.append(path.resolve())
-    for node_, deps_ in node_deps_pairs:
-        node = pathlib.Path(node_)
-        deps = list(map(pathlib.Path, deps_))
-        if node.resolve() == path.resolve():
-            for dep in deps:
-                if basedir.resolve() in dep.resolve().parents:
-                    res_deps.append(dep.resolve())
-            break
+    finder = modulefinder.ModuleFinder(path=[str(basedir)])
+    # find files imported by `path`
+    finder.run_script(str(path))
+    for module in finder.modules.values():
+        if module.__file__ is None:  #exclude modules that don't have source file (like sys).
+            continue
+        file = pathlib.Path(module.__file__)
+        try:  # check under `basedir`
+            file.relative_to(basedir)
+            res_deps.append(file)
+        except ValueError:
+            pass
     return list(set(res_deps))
 
 
